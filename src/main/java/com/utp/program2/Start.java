437f3f4bac5949ef74f1cc6c0b365708ddca3658
danjedradee <dnljss01@gmail.com>
2024-10-29 20:54:41 -0500

    Update Start.java

--- src/main/java/com/utp/program2/Start.java
+++ src/main/java/com/utp/program2/Start.java
@@ -1,5 +1,17 @@
 package com.utp.program2;
 
-public class Start {
-  // Pendiente de implementar
-}
+public class Program_2 {
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de vendedores:"));
+        int m = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de meses:"));
+
+        int[][] ventas = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ventas[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las ventas del vendedor " + (i + 1) + " para el mes " + (j + 1) + ":"));
+            }
+        }
+
+        Impresion.mostrarOpciones(ventas);
+    }
+
