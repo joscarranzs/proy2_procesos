1b58725d0583c13b6dfa33a1a83b4ffcee35aee0
danjedradee <dnljss01@gmail.com>
2024-10-29 21:48:12 -0500

    Update Message.java

--- src/main/java/com/utp/program2/messages/Message.java
+++ src/main/java/com/utp/program2/messages/Message.java
@@ -1,5 +1,86 @@
-package com.utp.program2.messages;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.program_2;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author dalbe
+ */
 
-public class Message {
-  // Pendiente de implementar
+public class Impresion {
+    public static void imprimirMatriz(int[][] ventas) {
+        String[] columnNames = new String[ventas[0].length];
+        for (int i = 0; i < columnNames.length; i++) {
+            columnNames[i] = "Mes " + (i + 1);
+        }
+
+        String[][] data = new String[ventas.length][ventas[0].length];
+        for (int i = 0; i < ventas.length; i++) {
+            for (int j = 0; j < ventas[i].length; j++) {
+                data[i][j] = String.valueOf(ventas[i][j]);
+            }
+        }
+
+        JTable table = new JTable(data, columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
+        JOptionPane.showMessageDialog(null, scrollPane, "Ventas Mensuales por Vendedor", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void imprimirVector(int[] vector, String titulo) {
+        StringBuilder sb = new StringBuilder(titulo + "\n");
+        for (int i = 0; i < vector.length; i++) {
+            sb.append("Vendedor ").append(i + 1).append(": ").append(vector[i]).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, sb.toString(), titulo, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void imprimirVector(double[] vector, String titulo) {
+        StringBuilder sb = new StringBuilder(titulo + "\n");
+        for (int i = 0; i < vector.length; i++) {
+            sb.append("Vendedor ").append(i + 1).append(": ").append(vector[i]).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, sb.toString(), titulo, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void mostrarOpciones(int[][] ventas) {
+        String[] opciones = {
+            "Mostrar Matriz",
+            "Ventas por Vendedor",
+            "Promedio por Vendedor",
+            "Ventas por Mes",
+            "Promedio por Mes",
+            "Totalizar Ventas"
+        };
+
+        JComboBox<String> comboBox = new JComboBox<>(opciones);
+        int seleccion = JOptionPane.showConfirmDialog(null, comboBox, "Seleccione una opción", JOptionPane.OK_CANCEL_OPTION);
+
+        if (seleccion == JOptionPane.OK_OPTION) {
+            String opcion = (String) comboBox.getSelectedItem();
+            switch (opcion) {
+                case "Mostrar Matriz":
+                    imprimirMatriz(ventas);
+                    break;
+                case "Ventas por Vendedor":
+                    imprimirVector(Calculos.calcularVtasPorVendedor(ventas), "Ventas por Vendedor");
+                    break;
+                case "Promedio por Vendedor":
+                    imprimirVector(Calculos.calcularPromPorVendedor(ventas), "Promedio por Vendedor");
+                    break;
+                case "Ventas por Mes":
+                    imprimirVector(Calculos.calcularVtasPorMes(ventas), "Ventas por Mes");
+                    break;
+                case "Promedio por Mes":
+                    imprimirVector(Calculos.calcularPromPorMes(ventas), "Promedio por Mes");
+                    break;
+                case "Totalizar Ventas":
+                    int total = Calculos.totalizarVentas(ventas);
+                    JOptionPane.showMessageDialog(null, "Total de Ventas: " + total, "Totalizar Ventas", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+            }
+        }
+    }
 }
